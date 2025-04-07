5336b2b7b19f9a4274e974b0929569ee78e3389d
AztKloyd <kloyd007@gmail.com>
2025-04-08 00:07:03 +0900

    initial commit for frontend

---
+++ src/pages/Home.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+import foodRecipe from '../assets/foodRecipe.png'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import RecipeItems from '../components/RecipeItems'
+import { useNavigate } from 'react-router-dom'
+import Modal from '../components/Modal'
+import InputForm from '../components/InputForm'
+
+export default function Home() {
+    const navigate = useNavigate()
+    const [isOpen, setIsOpen] = useState(false)
+
+    const addRecipe = () => {
+        let token = localStorage.getItem("token")
+        if (token)
+            navigate("/addRecipe")
+        else {
+            setIsOpen(true)
+        }
+    }
+
+    return (
+        <>
+            <section className='home'>
+                <div className='left'>
+                    <h1>Food Recipe</h1>
+                    <h5>레시피 저장 공간</h5>
+                    <button onClick={addRecipe}>Share your recipe</button>
+                </div>
+                <div className='right'>
+                    <img src={foodRecipe} width="320px" height="300px"></img>
+                </div>
+            </section>
+
+            {(isOpen) && <Modal onClose={() => setIsOpen(false)}><InputForm setIsOpen={() => setIsOpen(false)} /></Modal>}
+            <div className='recipe'>
+                <RecipeItems />
+            </div>
+            <div className='bg'>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#FFF8DC" fill-opacity="1" d="M0,128L80,106.7C160,85,320,43,480,64C640,85,800,171,960,202.7C1120,235,1280,213,1360,202.7L1440,192L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg>
+            </div>
+        </>
+    )
+}
